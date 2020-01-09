13181b30e5c2bd2862f547343d270c002cddfd02
Jason Melton <jason.melton2@gmail.com>
2020-01-09 08:05:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -89,8 +89,6 @@ matz = {
 }
 
 programmer_hash.merge!(matz)
-
-    
 end
 
 def changing_alan
@@ -112,6 +110,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+alans_new_info ="GUI"
+programmer_hash[:alan_kay][:known_for] = alans_new_info
      
 end
 
