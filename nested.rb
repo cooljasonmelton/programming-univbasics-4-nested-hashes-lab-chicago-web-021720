e6e15e713af4802f522701848aae25ff8cd0af98
Jason Melton <jason.melton2@gmail.com>
2020-01-09 07:46:42 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,8 +16,6 @@ def hopper
         }
      }
 
-programmer_hash[:]
-
 end
 
 def alan_kay_is_known_for
@@ -38,6 +36,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
